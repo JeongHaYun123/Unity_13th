952340209eef2b39ad04b80b06fffe9faf6e0f8b
JeongHaYun123 <gkdnswldbs@gmail.com>
2025-03-19 16:54:31 +0900

    함수(매서드), enum(열거형), 배열
    함수 (static 과 인스턴스 포함)
    enum 상태 (열거형)
    배열 (내일 골 도착 완료 예정)

---
+++ CSharpBasic/ArraySample/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace ArraySample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // new 키워드 : 새로 어떤 데이터가 초기화가 되었다는 뜻 //동적할당을 하기위한 키워드
+            // 배열 생성 방법 : new 자료형[크기]
+            // 배열은 참조 형식
+            //int[] arr = new int[5]; // 모든 요소 0 초기화
+            //int[] arr = { 3, 4, 2, 7, 1 }; // 요소별 초기값 주고싶을때
+            int[] arr = new int[5] { 3, 4, 2, 7, 1 }; // 이렇게도 쓸수 있음 //배열이 몇 개인지 보기 쉽게 하고, 초기 값 줄 수 있음
+
+            // 배열의 인덱서
+            // 배열에서 특정 번째 요소에 접근할 수 있도록 하는 기능
+            // arr[i] , 배열의 첫번째 주소에서부터
+            //          i * 요소의 자료형크기 만큼 뒤로가서 
+            //          해당 주소부터 자료형크기만큼 읽거나 쓰기 함.
+            arr[0] = 5;
+            arr[1] = 4;
+            arr[2] = 3;
+            arr[3] = 2;
+            arr[4] = 1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+
+            Console.WriteLine();
+
+            // Array 클래스
+            // 배열관련 편의 기능들이 있음
+            Array.Sort(arr); //오름차순 정렬
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+
+            Array.Reverse(arr); //내림차순 정렬
+            int indexOf2 = Array.IndexOf(arr, 2); //2가 있는 인덱스 반환
+            int[] arr2 = { 6, 2, 3, 4, 1, 2 };
+            Array.Copy(arr, 2, arr2, 0, 3); //arr의 2번째 인덱스부터 3개를 arr2의 0번째 인덱스부터 복사
+
+            // 2차원 배열
+            // 다차원 배열이라고해서 힙메모리영역에 다차원으로 할당하는게 아니고 그냥 연속적인 데이터로 할당을 한 다음
+            // 차원 인덱서를 통해 알맞는 위치에 접근하는 방식.
+            // ----------------------------------------------
+
+            // 0 : 길
+            // 1 : 벽
+            // 2 : 도착지점
+            // 3 : 플레이어
+            int[ , ] map = new int[6, 5] // 0차원이 y 축, 1차원이 x축 //6행 5열
+            {
+                { 0, 0, 0, 0, 1},
+                { 0, 1, 1, 1, 1},
+                { 0, 0, 0, 1, 1},
+                { 1, 1, 0, 1, 1},
+                { 1, 1, 0, 1, 1},
+                { 1, 1, 0, 0, 2},
+             };
+
+            MapNode[,] map2 = new MapNode[6, 5]
+            {
+                { MapNode.Path, MapNode.Path, MapNode.Path, MapNode.Path, MapNode.Wall},
+                { MapNode.Path, MapNode.Wall, MapNode.Wall, MapNode.Wall, MapNode.Wall},
+                { MapNode.Path, MapNode.Path, MapNode.Path, MapNode.Wall, MapNode.Wall},
+                { MapNode.Wall, MapNode.Wall, MapNode.Path, MapNode.Wall, MapNode.Wall},
+                { MapNode.Wall, MapNode.Wall, MapNode.Path, MapNode.Wall, MapNode.Wall},
+                { MapNode.Wall, MapNode.Wall, MapNode.Path, MapNode.Path, MapNode.Goal},
+            };
+        }
+
+        enum MapNode
+        {
+            None,
+            Path,
+            Wall,
+            Goal,
+            User
+        }
+    }
+}
--- CSharpBasic/EnumSample/Program.cs
+++ CSharpBasic/EnumSample/Program.cs
@@ -4,17 +4,46 @@
 
 namespace EnumSample
 {
-    enum State
+    /*
+     * C# 의 enum 타입은 기본적으로 int 값으로 취급
+     * enum 정의시 0 값 자리는 의미없는 default 로 설정함. ( idle, None, Nothing, Default .. 등등)
+     */
+    enum State : ushort //천 단위까지 가능, 일반적인 열거형은 단수 비트 열거형은 복수
     {
         None,
         Move,
         inAir,
-        Attack,
+        Attack = 100,
+        Skill1,
+        Skill2,
+    }
+
+    [Flags] // 현재 enum 의 ToString() 으로 문자열 반환하는 함수에서 모든 플래그의 요소들을 문자열로 바꿔주는 특성을 부여
+    enum Layers
+    {
+        Nothing       = 0 << 0, // 0b... 00000000 // 0b... 00000000   //0번 째는 의미 없는 걸로 채우는게 원칙 ( 아무것도 안 했는데 그라운드 되기 좀 그래서 0번째는 아무것도 안하는걸로 채움 )
+        Ground        = 1 << 0, // 0b... 00000001 // 0b... 00000001   //비트 플래그를 사용하지 않으면 연산 값이 이상하게 나온다 ( Ground + Enemy = Player 가 나옴 )
+        Enemy         = 1 << 1, // 0b... 00000010 // 0b... 00000010
+        Player        = 1 << 2, // 0b... 00000100 // 0b... 00000011
+        Interactable  = 1 << 3, // 0b... 00001000 // 0b... 00000100
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Layers targetLayer = Layers.Enemy | Layers.Interactable;
+
+            Layers collisionLayerMask = Layers.Ground | Layers.Enemy;
+
+            // 충돌가능
+            if ((targetLayer & collisionLayerMask) > 0)
+            {
+                // 충돌 처리
+            }
+
+            //Console.WriteLine(Layers.Ground.ToString());
+            //Console.WriteLine((Layers.Ground | Layers.Enemy).ToString());
+
             while (true)
             {
                 State currentState = (State)Enum.Parse(typeof(State), Console.ReadLine()); //Eunm 클래스 : enum 타입 관련 편의 기능들을 가지고 있음.
