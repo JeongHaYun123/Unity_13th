5f004c27175717d9743678ef68f8c06d70db31c8
JeongHaYun123 <gkdnswldbs@gmail.com>
2025-03-31 17:40:14 +0900

    동적 배열

---
+++ CSharpBasic/CollectionsSample/DynamicArray'1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CollectionsSample
+{
+    class DynamicArray_1<T>
+    {
+        public DynamicArray_1(int capactity)
+        {
+            _data = new T [capactity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if(index >= _count ||  index < 0)
+                    throw new IndexOutOfRangeException();
+
+                return _data[index];
+            }
+            set
+            {
+                if (index >= _count || index < 0)
+                    throw new IndexOutOfRangeException();
+
+                _data[index] = value;
+            }
+        }
+
+        public int Count => _count;
+
+        public int Capactity
+        {
+            get => _data.Length;
+            set
+            {
+                if (_count > value)
+                    throw new Exception("현재 아이템 수보다 작은 공간으로 만들 수 없습니다.");
+
+                T [] temp = new T [value];
+
+                for (int i = 0; i < _count; i++)
+                {
+                    temp [i] = _data [i];
+                }
+
+                _data = temp;
+            }
+        }
+
+        public T[] _data;
+
+        public int _count;
+
+        public void Add(T[] item)
+        {
+            if (_count == _data.Length)
+            {
+                T[] temp = new T[_count * 2];
+
+
+                for (int i = 0; i < _count; i++)
+                {
+                    temp[i] = _data [i];
+                }
+
+                _data = temp;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0  || index >= _count) throw new IndexOutOfRangeException();
+
+            for (index = 0;  index < _count - 1; index++)
+            {
+                _data[index] = _data[index + 1];
+            }
+
+            _count--;
+        }
+
+        public int FindIndex(Predicate<T> match)
+        {
+
+            for (int i = 0; i < _count; i++)
+            {
+                if (match(_data[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool Remove(Predicate<T> match)
+        {
+            int index = FindIndex(match);
+
+            if (index < 0)
+                return false;
+
+            RemoveAt(index); return true;
+        }
+
+        public int FindLastIndex(Predicate<T> match)
+        {
+
+            for (int i = _count - 1; i >= 0; i--)
+            {
+                if (match(_data[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool RemoveLast(Predicate<T> match)
+        {
+            int index = FindLastIndex(match);
+
+            if (index < 0)
+                return false;
+
+            RemoveAt(index); return true;
+        }
+
+    }
+}
---
+++ CSharpBasic/CollectionsSample/DynamicArray.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsSample
+{
+    class DynamicArray
+    {
+        public DynamicArray(int capactity) //capacity : 용량
+        {
+            _data = new object[capactity];
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index >= _count || index < 0)
+                    throw new IndexOutOfRangeException();
+
+                return _data[index];
+            }
+            set
+            {
+                if (index >= _count || index < 0)
+                    throw new IndexOutOfRangeException();
+
+                _data[index] = value;
+            }
+        }
+
+        public int Count => _count;
+
+        public int Capacity
+        {
+            get => _data.Length;
+            set
+            {
+                if (_count > value)
+                    throw new Exception("현재 아이템 수 보다 작은 용량으로 설정할수 없습니다.");
+
+                object[] temp = new object[value];
+
+                for (int i = 0; i < _count; i++)
+                {
+                    temp[i] = _data[i];
+                }
+
+                _data = temp;
+            }
+        }
+
+        private object[] _data;
+        private int _count;
+
+
+        public void Add(object item)
+        {
+            // 용량이 부족할때
+            if (_count == _data.Length)
+            {
+                object[] temp = new object[_count * 2]; // 크기 2배짜리 배열 생성
+                
+                // 기존 데이터 복사
+                for (int i = 0; i < _count; i++)
+                {
+                    temp[i] = _data[i];
+                }
+
+                _data = temp;
+            }
+
+            _data[_count++] = item;
+        }
+
+        /// <summary>
+        /// index 의 아이템 삭제를 위해 이후 아이템들을 모두 한칸씩 앞으로 당김
+        /// </summary>
+        /// <param name="index"> 삭제하려는 아이템 위치 </param>
+        /// <exception cref="IndexOutOfRangeException"> 인덱스 범위 초과 </exception>
+        public void RemoveAt(int index)
+        {
+            if (index >= _count || index < 0)
+                throw new IndexOutOfRangeException();
+
+            for (int i = index; i < _count - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+
+            _count--;
+        }
+
+
+        //public delegate bool Predicate2<in T>(T value); //자주 쓰니 아래처럼 C# 자체적으로 사용할 수 있게 만듬
+
+        public int FindIndex(Predicate<object> match)
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (match(_data[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public int FindLastIndex(Predicate<object> match)
+        {
+            for (int i =_count - 1; i >=0; i--)
+            {
+                if (match(_data[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public bool Remove(Predicate<object> match)
+        {
+            int index = FindIndex(match);
+
+            if (index < 0 )
+                return false;
+
+            RemoveAt(index);
+            return true;
+        }
+
+        public bool RemoveLast(Predicate<object> match)
+        {
+            int index = FindLastIndex(match);
+
+            if (index < 0)
+                return false;
+
+            RemoveAt(index);
+            return true;
+        }
+
+        //현실적으로 모든 변수의 함수를 만드는 것은 불가능
+        /* public int FindIndexBiggerThan90()
+         {
+             for (int i = 0; i < _count; i++)
+             {
+                 if (_data[i] > 90)
+                 {
+                     return i;
+                 }
+
+             }
+         }*/
+    }
+}
